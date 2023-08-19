1da2bf3ebe5b2de557afd9a4fc92a1bb751f3495
Jordell <rjordell@gmail.com>
2023-08-18 21:01:52 -0700

    displays audio features, project finally in a decent place

--- src/components/Track.js
+++ src/components/Track.js
@@ -11,11 +11,24 @@ function Track(props) {
                     <>
                         <img src={props.track.album.images[0].url} className="track_cover" alt="" />
                         Name: {props.track.name}
+                        <br/>
                         Album: {props.track.album.name}
+                        <br/>
                         Artist: {props.track.artists[0].name}
+                        <br/>
                         Song Popularity: {props.track.popularity}
+                        <br/>
                         Artist Popularity: {props.artist.popularity}
                     </>
+                    <div>
+                        Danceability: {props.audioFeatures.danceability}
+                        <br/>
+                        Energy: {props.audioFeatures.energy}
+                        <br/>
+                        Tempo: {props.audioFeatures.tempo}
+                        <br/>
+                        Valence: {props.audioFeatures.valence}
+                    </div>
 
         </div>
     );

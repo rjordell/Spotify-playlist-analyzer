b73ec5d8064cb43a207df6f1b7eaef28dbe984d5
Jordell <rjordell@gmail.com>
2023-08-20 12:41:55 -0700

    formatted the info shown in the track component in slightly more pleasing way wip

--- src/components/Track.js
+++ src/components/Track.js
@@ -19,6 +19,24 @@ function Track(props) {
         <div className="containerHeader">{props.track.popularity}</div>
         <div className="containerSubheader">{props.artist.popularity}</div>
       </div>
+      <div className="now-playing__side">
+        <div className="containerHeader">{props.artist.followers.total}</div>
+        <div className="containerSubheader">followers</div>
+      </div>
+      <div className="now-playing__side">
+        <div className="containerHeader">{props.audioFeatures.tempo}</div>
+        <div className="containerSubheader">tempo</div>
+      </div>
+      <div className="now-playing__side">
+        <div className="containerHeader">{props.audioFeatures.energy}</div>
+        <div className="containerSubheader">energy</div>
+      </div>
+      <div className="now-playing__side">
+        <div className="containerHeader">
+          {props.audioFeatures.danceability}
+        </div>
+        <div className="containerSubheader">danceability</div>
+      </div>
     </div>
   );
 }

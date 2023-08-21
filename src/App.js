8e9e78b92fa4291a9e07ac433456c3befd046c93
Jordell <rjordell@gmail.com>
2023-08-21 12:27:47 -0700

    created setSelectedPlaylistId in app.js with corresponding state variable

--- src/App.js
+++ src/App.js
@@ -1,40 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import WebPlayback from './components/player/WebPlayback'
-import Login from './components/Login'
-import MainBox from './components/MainBox';
-import LeftBoxes from './components/LeftBoxes';
-import './styles/App.css';
+import React, { useState, useEffect } from "react";
+import WebPlayback from "./components/player/WebPlayback";
+import Login from "./components/Login";
+import MainBox from "./components/MainBox";
+import LeftBoxes from "./components/LeftBoxes";
+import "./styles/App.css";
 
 function App() {
-
-  const [token, setToken] = useState('');
+  const [token, setToken] = useState("");
+  const [selectedPlaylistId, setSelectedPlaylistId] = useState(null);
 
   useEffect(() => {
-
     async function getToken() {
-      const response = await fetch('/auth/token');
+      const response = await fetch("/auth/token");
       const json = await response.json();
       setToken(json.access_token);
     }
 
     getToken();
-
   }, []);
 
-  if(token === ''){
-      return (<Login/>);
-  }
-  else{
-    return(
+  if (token === "") {
+    return <Login />;
+  } else {
+    return (
       <div className="App">
-          <LeftBoxes/>
-          <MainBox/>
-          <WebPlayback token={token}/>
+        <LeftBoxes onPlaylistClick={setSelectedPlaylistId} />
+        <MainBox selectedPlaylistId={selectedPlaylistId} />
+        <WebPlayback token={token} />
       </div>
     );
   }
-
 }
-
 
 export default App;
